e11ca55583f72082bd06841be179f761c303ce0c
W1llAn <wchimborazo2840@uta.edu.ec>
2025-11-16 20:23:15 -0500

    Actualizar configuración de Babel para incluir presets y plugins necesarios para Expo y NativeWind

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,18 @@
+// babel.config.js
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
-    plugins: ["nativewind/babel", "react-native-reanimated/plugin"],
+    presets: [
+      // Expo + NativeWind (jsxImportSource)
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      // NativeWind se usa como PRESET, no como plugin
+      "nativewind/babel",
+    ],
+    plugins: [
+      // Necesario para expo-router
+      require.resolve("expo-router/babel"),
+      // Para Reanimated 4 en Expo SDK 54 se usa worklets:
+      "react-native-worklets/plugin",
+    ],
   };
 };
