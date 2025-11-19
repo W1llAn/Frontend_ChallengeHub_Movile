361e38fb08f2e8eab3fcaf144219b873c9c1dce2
W1llAn <wchimborazo2840@uta.edu.ec>
2025-11-19 01:22:07 -0500

    Agregar índice de componentes UI reutilizables

---
+++ components/UI/index.ts
@@ -0,0 +1,12 @@
+/**
+ * UI Components Index
+ * Exporta todos los componentes UI reutilizables
+ */
+export { Card } from "./Card";
+export { Button } from "./Button";
+export { Input } from "./Input";
+export { Avatar } from "./Avatar";
+export { SectionHeader } from "./SectionHeader";
+export { LoadingSpinner } from "./LoadingSpinner";
+export { InfoRow } from "./InfoRow";
+export { Badge } from "./Badge";
