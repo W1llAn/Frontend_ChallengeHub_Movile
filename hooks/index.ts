9d456bee62bba8e1fbdf2e73d50d33a9ad5aa552
xxxMichael <mayckolchavez27@gmail.com>
2025-12-04 16:09:44 -0500

    feat: exportar el hook useReports en el índice de hooks

--- hooks/index.ts
+++ hooks/index.ts
@@ -6,3 +6,4 @@ export { useUsers } from "./useUsers";
 export { useFormValidation, validationRules } from "./useFormValidation";
 export { useCategories } from "./useCategories";
 export { useUserCategories } from "./useUserCategories";
+export { useReports } from "./useReports";
